3111a47b94b1e3f04ec594360af2953ae02bc47b
HeyLey <leila.xr@gmail.com>
2018-11-15 17:09:57 +0300

    update

--- utils/save_csv.py
+++ utils/save_csv.py
@@ -4,9 +4,14 @@ import tables
 
 def hits_to_csv(t, name):
     nHits, pmtID, hitTime, isDN = read_hits(t)
-    df = pd.DataFrame({ 'nHits': nHits, 'pmtID': pmtID, 
-                        'hitTime': hitTime, 'isDN': isDN
-                     })
+    df = pd.DataFrame(columns=['nHits', 'pmtID', 'hitTime', 'isDN'], index=[0])
+    for i in range(0, 1):
+        n = nHits[i]
+        for j in range(0, n):
+            df2 = pd.DataFrame({ 'nHits': n, 'pmtID': pmtID[i][j], 
+                                'hitTime': hitTime[i][j], 'isDN': isDN[i][j]
+                             })
+            df.append(df2)
     df.to_csv(name, index=False)
 
 
