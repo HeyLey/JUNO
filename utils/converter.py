bed3517acfbd0ea24e09a69771154f1a6778979b
HeyLey <leila.xr@gmail.com>
2018-11-15 15:27:27 +0300

    update

--- utils/converter.py
+++ utils/converter.py
@@ -3,6 +3,6 @@ from utils.save_csv import hits_to_csv, pos_to_csv, true_info_to_csv
 def convert(t1, t2, t3, t4, t5):
     hits_to_csv(t1, 'lpmt_hits.csv')
     hits_to_csv(t2, 'spmt_hits.csv')
-    pos_to_csv(t4, 'lpmt_hits.csv')
-    pos_to_csv(t5, 'spmt_hits.csv')
+    pos_to_csv(t4, 'lpmt_pos.csv')
+    pos_to_csv(t5, 'spmt_pos.csv')
     true_info_to_csv(t3, 'true_info.csv')
